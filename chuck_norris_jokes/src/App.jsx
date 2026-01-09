078f5b60ab3cf81afedb1dd7315085b25b9437f6
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-01-09 14:38:45 +0000

    mobile first css and btn funct

--- chuck_norris_jokes/src/App.jsx
+++ chuck_norris_jokes/src/App.jsx
@@ -2,42 +2,39 @@
 import { useEffect, useState } from 'react'; 
 import './App.css' 
 
-// usestate for joke, set joke 
 
 // separate use effect and call getdata inside it? 
 function App() { 
-  useEffect(()=> { 
-    try { 
-      const getData = async function () { 
+  const [joke, setJoke] = useState("");
+
+
+  const getData = async function () {   
+  try { 
         const response = await fetch("https://api.chucknorris.io/jokes/random"); 
         const data = await response.json(); 
         // console.log(data); 
-        selectJoke(data.value);
-      }; 
-    } catch (error) { 
+        setJoke(data.value);
+      } catch (error) { 
       console.error("Error fetching joke:", error); 
       console.log("An error occurred while fetching the joke. Please try again later."); 
-    } 
-  },[]) 
+    }}
+  
 
-  // make new component for seleteJoke or leave out?
-  const selectJoke = (value) => { 
-    console.log(value); 
-    jokeHere.innerText = value; 
-  }; 
-  
-    // convert event listener (getData onClick in button 
-  button.addEventListener("click", function () { 
-    console.log("button pressed"); 
-    getData(); 
-  }); 
-  
-  // add hooks in here for joke state and button click event
+  // handle getData side effect run
+  useEffect(() => {
+    getData();
+  },[])
+
+
   return ( 
-  <div class="joke-container"> 
-  <h2>Joke Under Here</h2> 
-  <button id="joke-button">Click for a Joke!</button> 
-  <p>api credit here</p> </div> ) 
-  } 
+  <div>
+    <h1>Chuck Norris Jokes</h1> 
+
+    <div className="joke-container"> 
+      <button onClick={getData}>Click for a Joke!</button> 
+      <p>{joke || "loading.... loading.."}</p> 
+    </div> 
+  </div>
+  )}
   
-  export default App;
+export default App;
