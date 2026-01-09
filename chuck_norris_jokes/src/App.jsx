700789b9f7d41e8c8735ba1b7754a726d6303e45
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-01-09 13:22:10 +0000

    convert to react

--- chuck_norris_jokes/src/App.jsx
+++ chuck_norris_jokes/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect, useState } from 'react'; 
+import './App.css' 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// usestate for joke, set joke 
 
-export default App
+// separate use effect and call getdata inside it? 
+function App() { 
+  useEffect(()=> { 
+    try { 
+      const getData = async function () { 
+        const response = await fetch("https://api.chucknorris.io/jokes/random"); 
+        const data = await response.json(); 
+        // console.log(data); 
+        selectJoke(data.value);
+      }; 
+    } catch (error) { 
+      console.error("Error fetching joke:", error); 
+      console.log("An error occurred while fetching the joke. Please try again later."); 
+    } 
+  },[]) 
+
+  // make new component for seleteJoke or leave out?
+  const selectJoke = (value) => { 
+    console.log(value); 
+    jokeHere.innerText = value; 
+  }; 
+  
+    // convert event listener (getData onClick in button 
+  button.addEventListener("click", function () { 
+    console.log("button pressed"); 
+    getData(); 
+  }); 
+  
+  // add hooks in here for joke state and button click event
+  return ( 
+  <div class="joke-container"> 
+  <h2>Joke Under Here</h2> 
+  <button id="joke-button">Click for a Joke!</button> 
+  <p>api credit here</p> </div> ) 
+  } 
+  
+  export default App;
