2a69b551a1f64e540189a2bdd311a49b4a635640
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2026-01-29 17:26:07 +0000

    js script commented out

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const body = document.querySelector("body");
-const button = document.querySelector("#joke-button");
-const banner = document.querySelector(".main-header");
-const jokeContainer = document.querySelector(".joke-container");
+// const body = document.querySelector("body");
+// const button = document.querySelector("#joke-button");
+// const banner = document.querySelector(".main-header");
+// const jokeContainer = document.querySelector(".joke-container");
 
 
-const getData = async function () {
-  const response = await fetch("https://api.chucknorris.io/jokes/random");
-  const data = await response.json();
-  //   console.log(data);
-  selectJoke(data.value);
-};
+// const getData = async function () {
+//   const response = await fetch("https://api.chucknorris.io/jokes/random");
+//   const data = await response.json();
+//   //   console.log(data);
+//   selectJoke(data.value);
+// };
 
-const selectJoke = (value) => {
-  console.log(value);
-};
+// const selectJoke = (value) => {
+//   console.log(value);
+//   jokeContainer.innerText = value;
+//   button.classList.add("joke-chosen");
+// };
 
-getData();
+// getData();
 
